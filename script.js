a938ef911f9b7583ee0017d4e428a45c520cab9b
Jason Bedini <jasonbedini@gmail.com>
2021-11-03 18:25:31 -0700

    set up framework of logic for library

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 myLibrary = [];
 
-/* variable that will help keep track of the index of each book, it will be incremented 
-each time a book is added */
+/* variable that will help keep track of the index of each book in the array, 
+it will be incremented each time a book is added */
 let position = 1;
 
 function book(title, author, pageCount, read) {
@@ -11,8 +11,31 @@ function book(title, author, pageCount, read) {
     this.read = read;
 };
 
+// !!!! maybe have function for displaying initial books in library if there are any !!!!
+
+
+// function that runs when 'add new book' button is clicked, prompts user for book info
+function getBookInfo() {
+    // prompt user, store info in variables
+
+    // call constructor function to create new book object
+
+    // push new object onto 'mylibrary' array
+
+};
 
 function addBookToLibrary() {
-    // create div element 
-    let newBook = document.createElement('div')
+    // create div element for new book
+    let newBook = document.createElement('div');
+    // add data attribute and class
+    newBook.setAttribute(data-key, `${position}`);
+    newBook.classList.add('book');
+    // populate div with info from object
+
+    // append book div 
+
+    // also need to append buttons for changing read status and removing book, add 'data-key'
+
+    // increment position variable at end
+
 };
