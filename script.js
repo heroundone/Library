9170795a8c7550dfbb67485793e990fcc2774089
Jason Bedini <jasonbedini@gmail.com>
2021-11-05 17:49:27 -0700

    new books can be added to the page

--- script.js
+++ script.js
@@ -16,12 +16,30 @@ function book(title, author, pageCount, read) {
 
 // event listener for new book button, makes form appear
 document.querySelector('#newBook button').addEventListener('click', () => {
-    let info = document.getElementById('form');
-    info.setAttribute('style', 'display:block;');
+    document.getElementById('form').style.display = 'block';
 });
 
 // event listeners for submit and cancel buttons within add new book form
-
+document.querySelector('#cancelButton').addEventListener('click', () => {
+    // hide new book form
+    document.getElementById('form').style.display = 'none';
+});
+document.querySelector('#submitButton').addEventListener('click', () => {
+    let title = document.querySelector('#title').value;
+    document.querySelector('#title').value = '';
+    let author = document.querySelector('#author').value;
+    document.querySelector('#author').value = '';
+    let pageCount = document.querySelector('#page').value;
+    document.querySelector('#page').value = '';
+    let read = document.querySelector('#read').value;
+    document.querySelector('#read').value = '';
+    let newBook = new book(title, author, pageCount, read);
+    // hide new book form
+    document.getElementById('form').style.display = 'none';
+    // add new object to mylibrary array
+    myLibrary.push(newBook);
+    addBookToLibrary(newBook);
+})
 
 // !!!! maybe have function for displaying initial books in library if there are any !!!!
 
@@ -38,16 +56,21 @@ function getBookInfo() {
 
 };
 
-function addBookToLibrary() {
+function addBookToLibrary(newBook) {
     // create div element for new book
-    let newBook = document.createElement('div');
+    let newBookDiv = document.createElement('div');
     // add data attribute and class
-    newBook.setAttribute(data-key, `${position}`);
-    newBook.classList.add('book');
+    newBookDiv.setAttribute('data-key', `${position}`);
+    newBookDiv.classList.add('book');
     // populate div with info from object
-
+    for(const key in newBook) {
+        let newDiv = document.createElement('div');
+        newDiv.textContent = newBook[key];
+        newBookDiv.appendChild(newDiv);
+    }
     // append book div 
-
+    const bookcase = document.getElementById('bookcase');
+    bookcase.appendChild(newBookDiv);
     // also need to append buttons for changing read status and removing book, add 'data-key'
 
     // increment position variable
