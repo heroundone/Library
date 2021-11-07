6399dfab625fecd50540b89d3a3affffc1a42fa2
Jason Bedini <jasonbedini@gmail.com>
2021-11-06 17:21:09 -0700

    Can now remove books from the bookcase/library

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ myLibrary = [];
 
 /* variable that will help keep track of the index of each book in the array, 
 it will be incremented each time a book is added */
-let position = 1;
+let position = 0;
 
 // constructor for new book object
 function book(title, author, pageCount, read) {
@@ -19,12 +19,13 @@ document.querySelector('#newBook button').addEventListener('click', () => {
     document.getElementById('form').style.display = 'block';
 });
 
-// event listeners for submit and cancel buttons within add new book form
+// event listeners for submit and cancel buttons
 document.querySelector('#cancelButton').addEventListener('click', () => {
-    // hide new book form
+    // hide 'add new book' form
     document.getElementById('form').style.display = 'none';
 });
 document.querySelector('#submitButton').addEventListener('click', () => {
+    // upon submission all data is stored, and all form fields are cleared
     let title = document.querySelector('#title').value;
     document.querySelector('#title').value = '';
     let author = document.querySelector('#author').value;
@@ -33,6 +34,7 @@ document.querySelector('#submitButton').addEventListener('click', () => {
     document.querySelector('#page').value = '';
     let read = document.querySelector('#read').value;
     document.querySelector('#read').value = '';
+    // create new book object
     let newBook = new book(title, author, pageCount, read);
     // hide new book form
     document.getElementById('form').style.display = 'none';
@@ -44,22 +46,10 @@ document.querySelector('#submitButton').addEventListener('click', () => {
 // !!!! maybe have function for displaying initial books in library if there are any !!!!
 
 
-// function that runs when 'add new book' button is clicked, prompts user for book info
-function getBookInfo() {
-    /* prompt user, multiple input values with placeholder text, need confirm and cancel buttons
-    TRY IN HTML FIRST, store info in variables */
-
-
-    // call constructor function to create new book object, called by confirm button
-
-    // push new object onto 'mylibrary' array
-
-};
-
 function addBookToLibrary(newBook) {
     // create div element for new book
     let newBookDiv = document.createElement('div');
-    // add data attribute and class
+    // add data attribute and class, then add to array
     newBookDiv.setAttribute('data-key', `${position}`);
     newBookDiv.classList.add('book');
     // populate div with info from object
@@ -68,11 +58,47 @@ function addBookToLibrary(newBook) {
         newDiv.textContent = newBook[key];
         newBookDiv.appendChild(newDiv);
     }
+    // set up buttons for changing read status and removing book, add 'data-key' attribute
+    let newSpan = document.createElement('span');
+
+    let removeButton = document.createElement('button');
+    removeButton.setAttribute('data-key', `${position}`);
+    removeButton.addEventListener('click', (e) => removeBook(e));
+    removeButton.textContent = 'Remove';
+
+    let readStatusButton = document.createElement('button');
+    readStatusButton.setAttribute('data-key', `${position}`);
+    readStatusButton.textContent = 'Mark as Read/Unread';
+
+    newSpan.appendChild(removeButton);
+    newSpan.appendChild(readStatusButton);
+    newBookDiv.appendChild(newSpan);
+
     // append book div 
     const bookcase = document.getElementById('bookcase');
     bookcase.appendChild(newBookDiv);
-    // also need to append buttons for changing read status and removing book, add 'data-key'
 
     // increment position variable
     position++;
+};
+
+function removeBook(event) {
+    // need array to store the html of each book for modification
+    let bookDivs = Array.from(document.querySelectorAll('.book'));
+
+    parent = event.target.parentNode.parentNode;
+    indexOfBook = parent.getAttribute('data-key');
+    parent.remove();
+
+    // remove the book from both arrays
+    myLibrary.splice(indexOfBook, 1);
+    bookDivs.splice(indexOfBook, 1);
+    position--;
+    let i = 0;
+
+    //increment position of books, that are after the deletion, down one
+    bookDivs.forEach(div => {
+        div.setAttribute('data-key', `${i}`);
+        i++
+    });
 };
